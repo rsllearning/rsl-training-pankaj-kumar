ca41fe41fa465b9756b511384a586d29e2b739a0
Pankaj kumar <pankaj.kumar@rajasoftwarelabs.com>
2026-09-16 11:53:39 +0530

    boilerplate code for TDD assignment

---
+++ src/main/java/com/rsl/training/subscription/SubscriptionPricingApplication.java
@@ -0,0 +1,12 @@
+package com.rsl.training.subscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionPricingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptionPricingApplication.class, args);
+    }
+}
